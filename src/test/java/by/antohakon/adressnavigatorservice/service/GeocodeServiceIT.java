67430e4a969a289a1480cf669eda2f810b7d04be
AntonP17 <pokrovskii.anton@mail.ru>
2025-08-16 22:45:28 +0300

    add IT

--- src/test/java/by/antohakon/adressnavigatorservice/service/GeocodeServiceIT.java
+++ src/test/java/by/antohakon/adressnavigatorservice/service/GeocodeServiceIT.java
@@ -1,12 +1,20 @@
 package by.antohakon.adressnavigatorservice.service;
 
+import by.antohakon.adressnavigatorservice.dto.AddressNavigationResponseDto;
+import by.antohakon.adressnavigatorservice.dto.DaDataApiResponse;
 import by.antohakon.adressnavigatorservice.dto.RequestAddressDto;
 import by.antohakon.adressnavigatorservice.entity.AddressDistantionEntity;
+import by.antohakon.adressnavigatorservice.mapper.AddressNavigationMapper;
 import by.antohakon.adressnavigatorservice.repository.AddressNavigationRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -16,14 +24,26 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
 
-@Testcontainers
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
+@Testcontainers
 public class GeocodeServiceIT {
 
-
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // почитать про эти тест контейнеры подробней
     // почему не подтягивается экземпляр репозитория
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     @Autowired
     private GeocodeService geocodeService;
@@ -34,6 +54,9 @@ public class GeocodeServiceIT {
     private HttpClient httpClient;
     @MockitoBean
     private ObjectMapper objectMapper;
+    @MockitoBean
+    private AddressNavigationMapper addressNavigationMapper;
+
 
     @Container
     static PostgreSQLContainer<?> postgreSQLContainer =
@@ -65,20 +88,50 @@ public class GeocodeServiceIT {
                 .distantion(2.2)
                 .build();
 
-        String jsonResponse = """
-                [{
-                    "result": "Спб, Олеко Дундича 5",
-                    "geo_lat": "60.0",
-                    "geo_lon": "30.0",
-                    "qc_geo": "0"
-                }]
-                """;
+        addressNavigationRepository.findByAddress(requestAddressDto.address());
 
-    //    addressNavigationRepository.save(addressDistantionEntity);
-//
-//        when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
-//                .thenReturn(jsonResponse);
+        daDataResponseMock(requestAddressDto.address(), "60.0", "30.0");
+
+        when(addressNavigationMapper.toDto(addressDistantionEntity))
+                .thenReturn(
+                        AddressNavigationResponseDto.builder()
+                                .id(addressDistantionEntity.getId())
+                                .address(addressDistantionEntity.getAddress())
+                                .distantion(addressDistantionEntity.getDistantion())
+                                .build()
+                );
+
+        AddressNavigationResponseDto response = geocodeService.processAddress(requestAddressDto);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(requestAddressDto.address(), response.getAddress());
+        Assertions.assertEquals(addressDistantionEntity.getDistantion(), response.getDistantion());
 
     }
 
+    @SneakyThrows
+    private void daDataResponseMock(String address, String lat, String lon) {
+
+        HttpResponse<String> response = Mockito.mock(HttpResponse.class);
+        String jsonResponse = String.format("""
+                [{
+                    "result": "%s",
+                    "geo_lat": "%s",
+                    "geo_lon": "%s",
+                    "qc_geo": "0"
+                }]
+                """, address, lat, lon);
+
+        when(response.body()).thenReturn(jsonResponse);
+
+        DaDataApiResponse daDataResponse = new DaDataApiResponse();
+        daDataResponse.setFormattedAddress(address);
+        daDataResponse.setLatitude(lat);
+        daDataResponse.setLongitude(lon);
+
+        when(httpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
+                .thenReturn(response);
+        when(objectMapper.readValue(anyString(), ArgumentMatchers.<TypeReference<List<DaDataApiResponse>>>any()))
+                .thenReturn(List.of(daDataResponse));
+    }
 }
